bd81a4cd0ead8181ae4aea697dd788a353dcca4c
vgrau98 <victor.grau93@gmail.com>
2022-04-25 15:04:05 +0200

    update config and helloworld files

--- todos/helloworld.js
+++ todos/helloworld.js
@@ -20,8 +20,8 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                // message: 'hello world default',
-                event
+                message: 'hello world UAT environment',
+                // event
                 // location: ret.data.trim()
             })
         }
