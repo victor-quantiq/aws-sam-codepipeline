9cb08bdc1f1eb909c2155334ca715a93d6123ad7
vgrau98 <victor.grau93@gmail.com>
2022-04-25 15:22:43 +0200

    fit: new feature in the helloworld function

--- todos/helloworld.js
+++ todos/helloworld.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world dev environment database updated manually',
+                message: 'hello world \n A new feature to promote to the production environment',
                 // event
                 // location: ret.data.trim()
             })
