952fcf15c5edb082df64a0d247fc3a0adf3e5e60
vgrau98 <victor.grau93@gmail.com>
2022-04-28 11:50:45 +0200

    test

--- todos/helloworld2.js
+++ todos/helloworld2.js
@@ -19,7 +19,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world 2 test \n A new revolutionary function to deploy to the production environment',
+                message: 'hello world 2 test2 \n A new revolutionary function to deploy to the production environment',
                 anotherMessage: 'another test message to promote to UAT environment'
             })
         }
